f3d25c26b872f8a827a000e3dc3ad49bf39ede5c
Samuel Murphy <samuel.murphy@gmail.com>
2018-05-18 01:28:26 -0400

    add bucket loop

--- blackblaze.py
+++ blackblaze.py
@@ -2,14 +2,23 @@ import sys
 import base64
 import json
 import urllib2
+import ConfigParser
 
-#id_and_key = 'hexAccountId_value:accountKey_value'
-id_and_key = 'e998c5598bb5:001941f0ac13642fa524fd8b9a2041e3ed3c605955'
+print("---------------------------------")
+Config = ConfigParser.ConfigParser()
+Config
+cfg = Config.read("config.txt")
+Config.sections()
+
+id = Config.get("blackblaze", "blackblaze_id")
+bl_key = Config.get("blackblaze", "blackblaze_appkey")
+id_and_key = id + ":" + bl_key
 
 basic_auth_string = 'Basic ' + base64.b64encode(id_and_key)
 headers = { 'Authorization': basic_auth_string }
 
 act_auth_token = ""
+api_url = ""
 
 request = urllib2.Request(
     'https://api.backblazeb2.com/b2api/v1/b2_authorize_account',
@@ -19,6 +28,7 @@ response = urllib2.urlopen(request)
 response_data = json.loads(response.read())
 
 act_auth_token = response_data['authorizationToken']
+api_url = response_data['apiUrl']
 
 response.close()
 
@@ -30,11 +40,30 @@ print 'minimum part size:', response_data['minimumPartSize']
 # get buckets
 
 
+
 request = urllib2.Request(
     '%s/b2api/v1/b2_list_buckets' % api_url,
-    json.dumps({ 'accountId' : account_id }),
+    json.dumps({ 'accountId' : id }),
     headers = { 'Authorization': act_auth_token }
     )
 response = urllib2.urlopen(request)
 response_data = json.loads(response.read())
 response.close()
+
+#print json.dumps(response_data, indent=4)
+
+for bucket in response_data["buckets"]:
+    print bucket["bucketName"]
+    bucket_id = bucket["bucketId"]
+
+    request = urllib2.Request(
+        '%s/b2api/v1/b2_list_file_names' % api_url,
+        json.dumps({ 'bucketId' : bucket_id }),
+        headers = { 'Authorization': act_auth_token }
+        )
+
+    response = urllib2.urlopen(request)
+    response_data = json.loads(response.read())
+    response.close()
+    print response_data
+
