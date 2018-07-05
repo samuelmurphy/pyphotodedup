a0cc8f70543a4e54f1caaf26dc780aa64f1c93b3
sam <samuel.murphy@gmail.com>
2018-07-05 10:20:45 -0400

    thoughts

--- local_windows.py
+++ local_windows.py
@@ -3,23 +3,32 @@ import os
 import re
 
 a = Path("c:/Users/Millie/Desktop")
+#a = Path("c:/")
 
 todo = []
 dirs_with_img = []
 file_types = "jpg$|mov$"  #move to config
 
 
+
+
 for curdir, sdrs, fils in os.walk(a):
     for f in fils:
         #fn0, ex = os.path.splitext(f)
         if re.search(file_types, f, re.IGNORECASE):
-            dirs_with_img.append(f)
+            dirs_with_img.append(curdir)
             break
 
-
+#print(dirs_with_img)
 print(len(dirs_with_img))
 
-
+for pth in dirs_with_img:
+    for fil in os.listdir(pth):
+        f = os.path.join(pth, fil)
+        if re.search(file_types, f, re.IGNORECASE):
+            sz = os.stat(f)[6]
+            print(fil + "  " + pth)
+#            print(sz)
         
     
         
